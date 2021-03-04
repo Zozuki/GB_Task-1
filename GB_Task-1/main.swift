983c39f949a513e5d8ac3ef34bb0013a1ef77251
Nick <untwsky@yandex.ru>
2021-03-04 23:36:04 +0300

    .

--- GB_Task-1/main.swift
+++ GB_Task-1/main.swift
@@ -7,5 +7,34 @@
 
 import Foundation
 
-print("Hello, World!")
+func quadratic(a: Double, b: Double, c: Double) {
+    let x1 = (-b - sqrt( pow(b, 2.0)  - 4 * a * c)) / (2 * a)
+    let x2 = (-b + sqrt( pow(b, 2.0)  - 4 * a * c)) / (2 * a)
+    print(" x1 = \(x1) \n x2 = \(x2)")
+}
+
+quadratic(a: 2, b: 4, c: 1)
+
+func rectTriangle(a: Double, b: Double) {
+    let square = (a * b) / 2
+    let perimeter = sqrt(pow(a, 2.0) + pow(b, 2.0)) + a + b
+    let hypo = sqrt(pow(a, 2.0) + pow(b, 2.0))
+    print(" Площадь: \(square) \n Периметр: \(perimeter) \n Гипотенуза: \(hypo)")
+}
+
+rectTriangle(a: 30, b: 40)
+
+func fiveYearPercent(dep: Double, percent: Double) {
+    var newDep = dep
+    var percents = 0.0
+    for _ in 1...5 {
+        let depPercents = newDep / 100 * percent
+        newDep += depPercents
+        percents += depPercents
+    }
+    let sumDep = dep + percents
+    print("Сумма вклада через 5 лет: \(sumDep), с \(percent)% за год")
+}
+
+fiveYearPercent(dep: 1000, percent: 10)
 
